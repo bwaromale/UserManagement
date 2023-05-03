549bf024f7fb48963dcc82461c26f7e5050b28c6
bwaromale <bwaromalelucky@gmail.com>
2023-05-03 10:19:56 +0100

    Modified Program.cs

--- UserManagement/UserManagement_Presentation/Program.cs
+++ UserManagement/UserManagement_Presentation/Program.cs
@@ -1,4 +1,9 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.Filters;
+using System.Text;
 using UserManagement_DataAccess;
 using UserManagement_DataAccess.InterfacesImplementation;
 using Usermanagement_Domain.Interfaces;
@@ -21,6 +26,31 @@ builder.Services.AddStackExchangeRedisCache(options => {
 builder.Services.AddScoped<IRepository<User>, Repository<User>>();
 builder.Services.AddScoped<IUser, UserRepository>();
 builder.Services.AddScoped<ICaching, CachingRepository>();
+
+builder.Services.Configure<ApiSettings>(builder.Configuration.GetSection("ApiSettings"));
+builder.Services.AddSwaggerGen(options =>
+{
+    options.AddSecurityDefinition("oauth2", new OpenApiSecurityScheme
+    {
+        Description = "Standard Authorization Header using the Bearer Scheme(\"Bearer {token}\")",
+        In = ParameterLocation.Header,
+        Name = "Authorization",
+        Type = SecuritySchemeType.ApiKey
+    });
+    options.OperationFilter<SecurityRequirementsOperationFilter>();
+});
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(options =>
+{
+    options.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuerSigningKey = true,
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(
+            builder.Configuration.GetSection("ApiSettings:SecretKey").Value)),
+        ValidateIssuer = false,
+        ValidateAudience = false,
+
+    };
+});
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -31,6 +61,8 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.UseAuthentication();
 
 app.UseAuthorization();
 
---
+++ UserManagement/Usermanagement_Domain/Models/ApiSettings.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Usermanagement_Domain.Models
+{
+    public class ApiSettings
+    {
+        public string SecretKey { get; set; }
+    }
+}
